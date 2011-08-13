f39309931b3fd288beb538ca16ac74f6020c3dcc
Dario Susnisky <dario15th@gmail.com>
2011-08-13 17:05:56 -0300

    Boludeces del parser probablemente a ser modificadas, pero no me deja hacer pull sino

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#include "./models/city.h"
+#include "./models/map.h"
+
+Map* parseMap(const char* path){
+    
+    FILE* mapfile;
+    Map* map;
+    int i;
+    City* cities;
+
+    if ((mapfile = fopen(path, "r")) == NULL){
+        return NULL;
+    }
+
+    if ((map = malloc(sizeof(Map))) == NULL){
+        return NULL;    
+    fscanf(mapfile, "%d\n", &(map->citycount));
+    fgetc(mapfile);
+    
+    if ((cities = malloc(n * sizeof(City))) == NULL){
+        return NULL; 
+    }
+    for (i = 0; i<map->citycount; i++){
+        fscanf(mapfile, "%s\n", cities[i].name);
+        cities[i].id = i;
+        cities[i].stockSize = 0;
+        while (fgetc(mapfile) != '\n'){
+            cities[i].stockSize++;
+            
+        }
+    }
+    return map;
+
+}
