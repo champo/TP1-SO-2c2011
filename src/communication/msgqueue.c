6e8ce80d2c359bf982dfcd44b71758fec8909254
Juan Pablo Civile <elementohb@gmail.com>
2011-09-06 21:55:43 -0300

    Fix issue with reading what isnt written

--- src/communication/msgqueue.c
+++ src/communication/msgqueue.c
@@ -11,7 +11,7 @@ struct MessageQueue {
     pthread_cond_t read;
     struct Message messages[QUEUE_SIZE];
     int first;
-    int last;
+    int tail;
 };
 
 struct MessageQueue* message_queue_create(void) {
@@ -25,7 +25,7 @@ struct MessageQueue* message_queue_create(void) {
     pthread_cond_init(&queue->read, NULL);
 
     queue->first = 0;
-    queue->last = 0;
+    queue->tail = 0;
 
     return queue;
 }
@@ -35,7 +35,7 @@ struct Message message_queue_pop(struct MessageQueue* queue) {
     struct Message msg;
 
     pthread_mutex_lock(&queue->mutex);
-    while (queue->first == queue->last) {
+    while (queue->first == queue->tail) {
         pthread_cond_wait(&queue->write, &queue->mutex);
     }
     msg = queue->messages[queue->first];
@@ -43,21 +43,24 @@ struct Message message_queue_pop(struct MessageQueue* queue) {
     pthread_cond_signal(&queue->read);
     pthread_mutex_unlock(&queue->mutex);
 
+    mprintf("Popped mesasge with type %d\n", msg.type);
+
     return msg;
 }
 
 void message_queue_push(struct MessageQueue* queue, struct Message msg) {
 
     pthread_mutex_lock(&queue->mutex);
-    while ((queue->last + 1) % QUEUE_SIZE == queue->first) {
+    while ((queue->tail + 1) % QUEUE_SIZE == queue->first) {
         pthread_cond_wait(&queue->read, &queue->mutex);
     }
 
-    queue->last = (queue->last + 1) % QUEUE_SIZE;
-    queue->messages[queue->last] = msg;
+    queue->messages[queue->tail] = msg;
+    queue->tail = (queue->tail + 1) % QUEUE_SIZE;
 
     pthread_cond_signal(&queue->write);
     pthread_mutex_unlock(&queue->mutex);
+    mprintf("Pushed message with type %d\n", msg.type);
 }
 
 void message_queue_destroy(struct MessageQueue* queue) {
