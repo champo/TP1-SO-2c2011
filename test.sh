13e4d5058993c0d0ff589b7792a224c0240caca9
Juan Civile <elementohb@gmail.com>
2011-09-09 03:11:10 -0300

    Improve the error detection by the script, now check for irregularties in the output

--- test.sh
+++ test.sh
@@ -7,12 +7,24 @@ for level in $(seq 1 3); do
         bin/sim configFiles/sim$level 2> out/err >> out/sim
         
         if [[ -s out/err ]]; then
-            echo "Error!"
+            echo "There's something in out/err. Bailing..."
             exit
         fi
         errs=`grep "INVALID" out/sim | wc -l | cut -d ' ' -f 1`
         if [[ $errs -ne 0 ]]; then
-            echo "Error!"
+            echo "Found $errs INVALID in out/sim"
+            exit
+        fi
+        biatch=`grep "biatch" out/sim`
+        if [[ -z $biatch ]]; then
+            echo "Didnt find a biatch signaling end..."
+            exit
+        fi
+
+        airlines=`ls configFiles/sim$level/airline* | wc -l | cut -d ' ' -f 1`
+        bye=`grep "Bye folks" out/sim | wc -l | cut -d ' ' -f 1`
+        if [[ $bye -ne $airlines ]]; then
+            echo "Not enough bye folks for every airline"
             exit
         fi
     done
