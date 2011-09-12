8e19eb034ef73867e76e10cd6eb703bdd2cddc35
Dario Susnisky <dario15th@gmail.com>
2011-09-12 01:26:57 -0300

    First version of the final output

--- src/app/output.c
+++ src/app/output.c
@@ -2,22 +2,54 @@
 #include "communication/types.h"
 #include <ncurses.h>
 
+#define AIRLINE_INITIAL_LINE 6
+#define TURN_LINE 2
+#define MAP_LINE_1 3
+#define MAP_LINE_2 4
+#define INITIAL_COLUMN 1
+#define FINAL_LINE_1 2
+#define FINAL LINE_2 3
 
 void run_output(struct MessageQueue* outputMsgQueue) {
-
-    //initscr();
-
+    //REMINDER: mvprintw(row,col,format,...)
+    initscr();
+    int turn = 0;
     struct Message msg;
+    int curLine;
+    int maxLine = AIRLINE_INITIAL_LINE;
+
     while ((msg = message_queue_pop(outputMsgQueue)).type != MessageTypeEndOutput) {
 
         if (msg.type == MessageTypeMapStatus) {
-           break; 
+            clean();
+            //Print number of turn
+            mvprintw(TURN_LINE, INITIAL_COLUMN,"Status in turn number %d.", turn);
+            turn++;
+            //Print map status
+            mvprintw(MAP_LINE_1, INITIAL_COLUMN, "%.2f%% of the needs in the map were already taken care of.", 
+                    msg.mapStatus.completionPercentage);
+            mvprintw(MAP_LINE_2, INITIAL_COLUMN, "The needs in %d out of %d total cities were already taken care of.",
+                    msg.mapStatus.citiesSatisfied, msg.mapStatus.totalCities);
+            refresh();
+            break; 
         } else if (msg.type == MessageTypeAirlineStatus) {
-           break;
+            //Print airline status
+            curLine = AIRLINE_INITIAL_LINE + msg.airlineStatus.id;
+            if ( curLine > maxLine ) {
+                maxLine = curLine;
+            }
+            mvprintw(curLine, INITIAL_COLUMN, "Airline number %d still has %d of its %d with some stock left.",
+                    msg.airlineStatus.id, msg.airlineStatus.planesFlying, msg.airlineStatus.totalPlanes);
+            refresh();
+            break;
         }
     }
-    
-    //TODO DECIR CHAU
-    //endwin();    
+   
+    mvprintw(maxLine + FINAL_LINE_1, INITIAL_COLUMN, 
+            "The simulation is over! The world is now safe from various diseases!");
+    mvprintw(maxLine + FINAL_LINE_2, INITIAL_COLUMN, 
+            "You must truly be a simulation ninja wizard :D"); 
+    refresh();
+    endwin();    
     return;
 }
